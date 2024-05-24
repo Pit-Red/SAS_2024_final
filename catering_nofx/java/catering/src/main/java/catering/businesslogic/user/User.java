be8f239814cc00a467fd6750f9fd640c201b2435
Gabriele Masciulli <gmasciu@gmail.com>
2024-05-24 21:47:20 +0200

    Aggiornato utente con il nuovo ruolo "COOK"

--- catering_nofx/java/catering/src/main/java/catering/businesslogic/user/User.java
+++ catering_nofx/java/catering/src/main/java/catering/businesslogic/user/User.java
@@ -12,13 +12,10 @@ import java.util.Set;
 
 public class User {
 
-    private static Map<Integer, User> loadedUsers = new HashMap<Integer, User>();
-
-    public static enum Role {SERVIZIO, CUOCO, CHEF, ORGANIZZATORE};
-
+    private static final Map<Integer, User> loadedUsers = new HashMap<Integer, User>();
+    private final Set<Role> roles;
     private int id;
     private String username;
-    private Set<Role> roles;
 
     public User() {
         id = 0;
@@ -26,44 +23,16 @@ public class User {
         this.roles = new HashSet<>();
     }
 
-    public boolean isChef() {
-        return roles.contains(Role.CHEF);
-    }
-
-    public String getUserName() {
-        return username;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String toString() {
-        String result = username;
-        if (roles.size() > 0) {
-            result += ": ";
-
-            for (User.Role r : roles) {
-                result += r.toString() + " ";
-            }
-        }
-        return result;
-    }
-
-    // STATIC METHODS FOR PERSISTENCE
-
     public static User loadUserById(int uid) {
         if (loadedUsers.containsKey(uid)) return loadedUsers.get(uid);
 
         User load = new User();
-        String userQuery = "SELECT * FROM Users WHERE id='"+uid+"'";
-        PersistenceManager.executeQuery(userQuery, new ResultHandler() {
-            @Override
-            public void handle(ResultSet rs) throws SQLException {
-                load.id = rs.getInt("id");
-                load.username = rs.getString("username");
-            }
+        String userQuery = "SELECT * FROM Users WHERE id='" + uid + "'";
+        PersistenceManager.executeQuery(userQuery, rs -> {
+            load.id = rs.getInt("id");
+            load.username = rs.getString("username");
         });
+
         if (load.id > 0) {
             loadedUsers.put(load.id, load);
             String roleQuery = "SELECT * FROM UserRoles WHERE user_id=" + load.id;
@@ -73,13 +42,13 @@ public class User {
                     String role = rs.getString("role_id");
                     switch (role.charAt(0)) {
                         case 'c':
-                            load.roles.add(User.Role.CUOCO);
+                            load.roles.add(User.Role.COOK);
                             break;
                         case 'h':
                             load.roles.add(User.Role.CHEF);
                             break;
                         case 'o':
-                            load.roles.add(User.Role.ORGANIZZATORE);
+                            load.roles.add(User.Role.EVENT_PLANNER);
                             break;
                         case 's':
                             load.roles.add(User.Role.SERVIZIO);
@@ -92,7 +61,7 @@ public class User {
 
     public static User loadUser(String username) {
         User u = new User();
-        String userQuery = "SELECT * FROM Users WHERE username='"+username+"'";
+        String userQuery = "SELECT * FROM Users WHERE username='" + username + "'";
 
         PersistenceManager.executeQuery(userQuery, new ResultHandler() {
             @Override
@@ -110,13 +79,13 @@ public class User {
                     String role = rs.getString("role_id");
                     switch (role.charAt(0)) {
                         case 'c':
-                            u.roles.add(User.Role.CUOCO);
+                            u.roles.add(User.Role.COOK);
                             break;
                         case 'h':
                             u.roles.add(User.Role.CHEF);
                             break;
                         case 'o':
-                            u.roles.add(User.Role.ORGANIZZATORE);
+                            u.roles.add(User.Role.EVENT_PLANNER);
                             break;
                         case 's':
                             u.roles.add(User.Role.SERVIZIO);
@@ -125,5 +94,42 @@ public class User {
             });
         }
         return u;
+    }
+
+    public boolean isChef() {
+        return roles.contains(Role.CHEF);
+    }
+
+    public boolean isCook() {
+        return roles.contains(Role.COOK);
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    // STATIC METHODS FOR PERSISTENCE
+
+    public String toString() {
+        String result = username;
+        if (roles.size() > 0) {
+            result += ": ";
+
+            for (User.Role r : roles) {
+                result += r.toString() + " ";
+            }
+        }
+        return result;
+    }
+
+    public enum Role {
+        SERVIZIO,
+        COOK,
+        CHEF,
+        EVENT_PLANNER
     }
 }
