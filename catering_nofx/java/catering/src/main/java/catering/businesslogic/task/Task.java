0c2c402c83779def54df562e7cffd8c73e1ccdde
Gabriele Masciulli <gmasciu@gmail.com>
2024-05-24 21:47:01 +0200

    Creata classe per il Task

--- catering_nofx/java/catering/src/main/java/catering/businesslogic/task/Task.java
+++ catering_nofx/java/catering/src/main/java/catering/businesslogic/task/Task.java
@@ -1,10 +1,94 @@
 package catering.businesslogic.task;
+
+import catering.businesslogic.procedure.CookingProcedure;
+import catering.businesslogic.shifts.KitchenShift;
+import catering.businesslogic.user.User;
+
 import java.time.Duration;
 
 public class Task {
     private Duration timeToComplete;
     private boolean completed;
+    private Boolean toPrepare;
     private String amount;
     private String doses;
-    private String toPrepare;
+    private CookingProcedure procedure;
+    private User cook;
+    private KitchenShift shift;
+
+    public Task(CookingProcedure procedure, KitchenShift shift, User cook) {
+        this.procedure = procedure;
+        this.shift = shift;
+        this.cook = cook;
+    }
+
+    public Task(CookingProcedure procedure, KitchenShift shift) {
+        this.procedure = procedure;
+        this.shift = shift;
+    }
+
+    // Getters and Setters
+    public Duration getTimeToComplete() {
+        return timeToComplete;
+    }
+
+    public void setTimeToComplete(Duration timeToComplete) {
+        this.timeToComplete = timeToComplete;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Boolean getToPrepare() {
+        return toPrepare;
+    }
+
+    public void setToPrepare(Boolean toPrepare) {
+        this.toPrepare = toPrepare;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getDoses() {
+        return doses;
+    }
+
+    public void setDoses(String doses) {
+        this.doses = doses;
+    }
+
+    public CookingProcedure getProcedure() {
+        return procedure;
+    }
+
+    public void setProcedure(CookingProcedure procedure) {
+        this.procedure = procedure;
+    }
+
+    public User getCook() {
+        return cook;
+    }
+
+    public void setCook(User cook) {
+        this.cook = cook;
+    }
+
+    public KitchenShift getShift() {
+        return shift;
+    }
+
+    public void setShift(KitchenShift shift) {
+        this.shift = shift;
+    }
 }
