82e520e7efa6f0bad0c652c61dbd8f4f3afb441e
Gabriele Masciulli <gmasciu@gmail.com>
2024-05-26 22:50:13 +0200

    Aggiornato caricamento foglio riepilogativo dal DB, aggiunta funzione di persistenza per la creazione e salvataggio di nuovi fogli

--- catering_nofx/java/catering/src/main/java/catering/businesslogic/task/SummarySheet.java
+++ catering_nofx/java/catering/src/main/java/catering/businesslogic/task/SummarySheet.java
@@ -14,25 +14,89 @@ import java.util.stream.Collectors;
 public class SummarySheet {
 
     private static final Map<Integer, SummarySheet> allSummarySheets = new HashMap<>();
-    private int id;
-    private ArrayList<Task> tasks;
-    private ArrayList<CookingProcedure> listedProcedures;
+    private final int id;
+    private final ArrayList<Task> tasks;
+    private final ArrayList<CookingProcedure> listedProcedures;
 
-    public SummarySheet(int id, ArrayList<CookingProcedure> procedures) {
-        this.id = id;
+    // Constructor for creating a new SummarySheet from a list of cooking procedures
+    public SummarySheet(ArrayList<Recipe> procedures) {
+        this.id = generateNewId();
         this.listedProcedures = new ArrayList<>(procedures);
         this.tasks = new ArrayList<>();
+        saveNewSummarySheet();
     }
 
-    public SummarySheet(int id) {
+    // Constructor for creating a new empty SummarySheet
+    public SummarySheet() {
+        this.id = generateNewId();
+        this.listedProcedures = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        saveNewSummarySheet();
+    }
+
+    // Constructor for loading an existing SummarySheet
+    private SummarySheet(int id) {
         this.id = id;
         this.listedProcedures = new ArrayList<>();
         this.tasks = new ArrayList<>();
     }
 
+    public static ArrayList<SummarySheet> loadAllSummarySheets() {
+        String query = "SELECT * FROM SummarySheets";
+        PersistenceManager.executeQuery(query, rs -> {
+            int id = rs.getInt("id");
+            if (!allSummarySheets.containsKey(id)) {
+                allSummarySheets.put(id, new SummarySheet(id));
+            }
+        });
+
+        String tasksQuery = "SELECT * FROM ListedTasks lt JOIN Tasks t ON lt.task_id = t.id";
+        PersistenceManager.executeQuery(tasksQuery, rs -> {
+            int id = rs.getInt("summary_sheet_id");
+            SummarySheet summarySheet = allSummarySheets.get(id);
+            if (summarySheet != null) {
+                summarySheet.addTask(Task.loadTaskById(rs.getInt("task_id")));
+            }
+        });
+
+        String proceduresQuery = "SELECT * FROM ListedProcedures lp JOIN CookingProcedures cp ON lp.procedure_id = cp.id";
+        PersistenceManager.executeQuery(proceduresQuery, rs -> {
+            int id = rs.getInt("summary_sheet_id");
+            SummarySheet summarySheet = allSummarySheets.get(id);
+            if (summarySheet != null) {
+                summarySheet.addProcedure(CookingProcedure.loadCookingProcedureById(rs.getInt("procedure_id")));
+            }
+        });
+
+        return new ArrayList<>(allSummarySheets.values());
+    }
+
+    public static ArrayList<SummarySheet> getAllSummarySheets() {
+        return new ArrayList<>(allSummarySheets.values());
+    }
+
+    private void saveNewSummarySheet() {
+        for (Task task : tasks) {
+            String taskQuery = "INSERT INTO ListedTasks (summary_sheet_id, task_id) VALUES (" + this.id + ", " + task.getId() + ")";
+            PersistenceManager.executeUpdate(taskQuery);
+        }
+
+        for (CookingProcedure procedure : listedProcedures) {
+            String procedureQuery = "INSERT INTO ListedProcedures (summary_sheet_id, procedure_id) VALUES (" + this.id + ", " + procedure.getId() + ")";
+            PersistenceManager.executeUpdate(procedureQuery);
+        }
+    }
+
+    private int generateNewId() {
+        String query = "INSERT INTO SummarySheets (id) VALUES (DEFAULT)";
+        PersistenceManager.executeUpdate(query);
+        return PersistenceManager.getLastId();
+    }
+
     public void addProcedure(CookingProcedure cookingProcedure) {
         this.listedProcedures.add(cookingProcedure);
     }
+
     public void addTask(Task task) {
         this.tasks.add(task);
     }
@@ -43,29 +107,6 @@ public class SummarySheet {
 
     @Override
     public String toString() {
-        return "SummarySheet {id:"+ this.id + ", Procedures:" + this.listedProcedures + "}";
-    }
-
-    // PERSISTENCE PART
-
-    public static ArrayList<SummarySheet> loadAllSummarySheet(){
-
-        String query = "SELECT * " +
-                "FROM ListedTasks l join Tasks t on l.task_id = t.id";
-
-        PersistenceManager.executeQuery(query, rs -> {
-            int id = rs.getInt("summary_sheet_id");
-            if (!allSummarySheets.containsKey(id))
-                allSummarySheets.put(id, new SummarySheet(id));
-            SummarySheet summarySheet = allSummarySheets.get(id);
-            summarySheet.addTask(Task.loadTastById(rs.getInt("task_id")));
-            summarySheet.addProcedure(CookingProcedure.loadCookingProcedureById(rs.getInt("cooking_procedure_id")));
-        });
-
-        return new ArrayList<>(allSummarySheets.values());
-    }
-
-    public static ArrayList<SummarySheet> getAllSummarySheets(){
-        return new ArrayList<>(allSummarySheets.values());
+        return "SummarySheet {id:" + this.id + ", Procedures:" + this.listedProcedures + "}";
     }
 }
